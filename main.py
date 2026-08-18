7a0a97ad9da8250c72ec21518b87b24cd45a750c
Piernik67 <twoj.email@github.com>
2026-08-18 14:11:01 +0200

    Poprawka: dodanie kodu szachowego do main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+import chess
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello World"}
+@app.get("/board", response_class=PlainTextResponse)
+def get_board(fen: str):
+    board = chess.Board(fen)
+    return str(board)
