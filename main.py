fef3b5975d73b7503ae7f9549ef46bb97103115a
Piernik67 <twoj.email@github.com>
2026-08-19 12:18:39 +0200

    two

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ import chess
 app = FastAPI()
 
 @app.get("/board", response_class=PlainTextResponse)
-def get_board(fen: str):
-    board = chess.Board(fen)
+def get_board(dwa: str):
+    board = chess.Board(dwa)
     return str(board)
