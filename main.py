9660b6e5c7cc6f3b771ebee9d2bd72d5e5d83234
Piernik67 <twoj.email@github.com>
2026-08-20 10:44:16 +0200

    Dodanie obslugi wielu gier ze slownikiem i UUID

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
 import chess
+import uuid
 
 app = FastAPI()
 
-@app.get("/board", response_class=PlainTextResponse)
-def get_board(dwa: str):
-    board = chess.Board(dwa)
-    return str(board)
+games: dict[str, chess.Board] = {}
+
+
+@app.post("/board")
+def new_game() -> str:
+    game_id = str(uuid.uuid4())
+    games[game_id] = chess.Board()
+    return game_id
+
+
+@app.get("/board/{game_id}", response_class=PlainTextResponse)
+def get_game(game_id: str) -> str:
+    try:
+        return str(games[game_id])
+    except KeyError:
+        return f"There is no game with the id: {game_id}"
